202e6028d36886c900e1a9934077d09e3fc02041
luohao <1223048059@qq.com>
2023-11-03 21:30:09 +0800

    feat: ch6 postgres db

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,32 @@
+import { sql } from "@vercel/postgres";
+
 export default function Page() {
-  return <p>Dashboard Page</p>;
+  return (
+    <>
+      <p>Dashboard Page</p>
+      <Cart
+        params={{
+          user: "",
+        }}
+      />
+    </>
+  );
+}
+
+async function Cart({
+  params,
+}: {
+  params: { user: string };
+}): Promise<JSX.Element> {
+  const { rows } = await sql`SELECT * from CARTS where user_id=${params.user}`;
+
+  return (
+    <div>
+      {rows.map((row) => (
+        <div key={row.id}>
+          {row.id} - {row.quantity}
+        </div>
+      ))}
+    </div>
+  );
 }
