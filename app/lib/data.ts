0f3b2c3944c1a26bf9d33e6b410189dbb97db3cd
luohao <1223048059@qq.com>
2023-11-04 16:21:56 +0800

    chore: delete delays

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -19,7 +19,6 @@ export async function fetchRevenue() {
     // Don't do this in real life :)
 
     console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 2500));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
@@ -34,8 +33,6 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
@@ -59,8 +56,6 @@ export async function fetchCardData() {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
     // how to initialize multiple queries in parallel with JS.
-
-    await new Promise((resolve) => setTimeout(resolve, 1500));
 
     const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
     const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
